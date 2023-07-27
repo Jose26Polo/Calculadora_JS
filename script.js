3096e362e1cd74420f688dee7d85607bc19ccd65
Jose Roberto Grego <jgrego63@gmail.com>
2023-07-27 17:50:32 -0300

    estrutura html e css da calculadora

---
+++ script.js
@@ -0,0 +1,9 @@
+const display = document.getElementById("display");
+const btnSubtract = document.getElementById("btnSub");
+const btnSum = document.getElementById("btnSum");
+const btnMultiply = document.getElementById("btnMul");
+const btnDivided = document.getElementById("btnDiv");
+const btnPoint = document.getElementById("btnPont");
+const btnClear = document.getElementById("btnClear");
+const btnEqual = document.getElementById("btnEqual");
+
